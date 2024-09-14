e2938a00d5a62a3515232cdbb8cd81e826ebf0b6
shangrex <hshangrex@gmail.com>
2024-09-14 16:59:21 -0500

    Add test all machine

--- mp1/src/server/sender_test.go
+++ mp1/src/server/sender_test.go
@@ -35,10 +35,56 @@ func TestOneMahine( t *testing.T){
         fmt.Printf("Error executing grep: %v\n", err)
     }
 
-    // Send the grep output back to the client
+    
 	local_responses := make([]string, 10)
     local_responses[1] = string(output)
 
+	// Compare the result
+	if reflect.DeepEqual(responses , local_responses) == false {
+		t.Errorf("Remote Output %+v is not equal to Local Expected Output %+v", responses, local_responses)
+	}
+
+}
+
+
+func TestAllMachine( t *testing.T){
+	/*
+	Test all machines and compare the grep's result from machine 1
+	*/
+	ip := "fa24-cs425-6602.cs.illinois.edu"
+	responses := make([]string, 10)
+
+	for i := 1; i < 10; i++ {
+		// Use Sprintf to embed the integer into a string
+		file_path := fmt.Sprintf("../../data/test_vm%d.log", i)
+		test_args := []string {"-c", "GET", file_path}
+
+		args := strings.Join(test_args, "\n")
+		// get distributed result
+		var wg sync.WaitGroup
+		wg.Add(1)
+		// ip := "localhost"
+		args += "\x00"
+		go connectAndSend(ip, i, args, &wg, responses)
+		wg.Wait()
+	}
+
+	local_responses := make([]string, 10)
+	// get local result
+	for i := 1; i < 10; i++ {
+		file_path := fmt.Sprintf("../../data/test_vm%d.log", i)
+		test_args := []string {"-c", "GET", file_path}
+		cmd := exec.Command("grep", test_args...)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("Error executing grep: %v\n", err)
+		}
+	
+		local_responses[i] = string(output)
+	}
+
+
+	// Compare the result
 	if reflect.DeepEqual(responses , local_responses) == false {
 		t.Errorf("Remote Output %+v is not equal to Local Expected Output %+v", responses, local_responses)
 	}
