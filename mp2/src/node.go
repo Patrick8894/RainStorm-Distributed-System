cda3edab0b1042fbbd4d31fceb9265b241e395fd
sjh9 <sjh9@fa24-cs425-6601.cs.illinois.edu>
2024-09-25 17:37:06 -0500

    Add mp2 file

---
+++ mp2/src/node.go
@@ -0,0 +1,14 @@
+func main(){
+	VMList := []string{
+		"fa24-cs425-6601.cs.illinois.edu",
+		"fa24-cs425-6602.cs.illinois.edu",
+		"fa24-cs425-6603.cs.illinois.edu",
+		"fa24-cs425-6604.cs.illinois.edu",
+		"fa24-cs425-6605.cs.illinois.edu",
+		"fa24-cs425-6606.cs.illinois.edu",
+		"fa24-cs425-6607.cs.illinois.edu",
+		"fa24-cs425-6608.cs.illinois.edu",
+		"fa24-cs425-6609.cs.illinois.edu",
+		"fa24-cs425-6610.cs.illinois.edu",
+	}
+}
