cfdc5f670be6feb265fccbaf39f536315a171ba8
shangrex <hshangrex@gmail.com>
2024-09-28 19:53:54 -0500

    Fix time

--- mp2/src/node.go
+++ mp2/src/node.go
@@ -305,7 +305,10 @@ func startClient() {
 		nodesArray[i], nodesArray[j] = nodesArray[j], nodesArray[i]
 	})
 
-	ticker := time.NewTicker(time.Duration(PROTOCOL_PERIOD) * time.Second) // Ping every PROTOCOL_PERIOD seconds
+    if PROTOCOL_PERIOD <= 0 {
+        fmt.Println("Invalid PROTOCOL_PERIOD value")
+    }
+	ticker := time.NewTicker(time.Duration(PROTOCOL_PERIOD) * float64(time.Second)) // Ping every PROTOCOL_PERIOD seconds
     defer ticker.Stop()
 
 	for {
