ca54eab7d82fe15c0d6170eeaf7948a558570bff
shangrex <hshangrex@gmail.com>
2024-09-29 16:09:32 -0500

    Fix

--- mp2/src/node.go
+++ mp2/src/node.go
@@ -144,7 +144,7 @@ func startHandlecommand() {
         } else if command == "lsg" {
             // send the list of nodes to the sender
             GossipNodesMutex.Lock()
-            jsonData, err := json.Marshal(GetGossiplist(global.GossipNodes))
+            jsonData, err := json.Marshal(global.GetGossiplist(global.GossipNodes))
             if err != nil {
                 fmt.Println("Error serializing data:", err)
                 os.Exit(1)
