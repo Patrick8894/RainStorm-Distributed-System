d2f32eafbc15840b761e7727e810b9b177bbf05a
shangrex <hshangrex@gmail.com>
2024-09-28 23:11:58 -0500

    Update time period

--- mp2/src/node.go
+++ mp2/src/node.go
@@ -29,8 +29,8 @@ import (
 
 var INTRODUCER_ADDRESS = "fa24-cs425-6605.cs.illinois.edu:8081"
 var PORT = "8081"
-var PROTOCOL_PERIOD = 0.5
-var TIMEOUT_PERIOD = 0.4
+var PROTOCOL_PERIOD = 1.0
+var TIMEOUT_PERIOD = 0.8
 var SUSPECT_TIMEOUT = 30
 
 var Introducer = false
