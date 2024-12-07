392c33a91d0bb2e7f99207dae5a69296b7672d72
bohaowu2 <bohaowu2@fa24-cs425-6601.cs.illinois.edu>
2024-12-07 16:30:44 -0600

    Add mp4 test

--- mp4/test1.sh
+++ mp4/test1.sh
@@ -1,3 +1,4 @@
 cd src
 # <op1_exe> <op2_exe> <hydfs_src_file> <hydfs_dest_filename> <num_tasks> <X> <stateful>
-go run client.go 
+go run client.go app1_op1.go app1_op2.go Traffic_Sign1000.txt business_3.txt 1 1 stateful
+go run client.go app1_op1.go app1_op2.go Traffic_Sign1000.txt business_3.txt 1 1 stateful
