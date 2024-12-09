587e08217c6f7e8a7f24b4080017346040e6cfc2
bohaowu2 <bohaowu2@fa24-cs425-6601.cs.illinois.edu>
2024-12-08 23:14:54 -0600

    UPdatet

--- mp4/test1.sh
+++ mp4/test1.sh
@@ -3,6 +3,6 @@ go run client.go create --localfilename ~/TrafficSigns_4.txt --HyDFSfilename Tra
 go run client.go create --localfilename ~/test1_1.txt --HyDFSfilename test1_1.txt
 cd ../../mp4/src
 # <op1_exe> <op2_exe> <hydfs_src_file> <hydfs_dest_filename> <num_tasks> <X> <stateful>
-go run client.go app1_op1 app1_op2 TrafficSigns_4.txt test1_1.txt 3 "No Outlet" stateless && 
-# go run client.go app2_op1.go app2_op2.go Traffic_Signs_1000.txt test2_2.txt 3 "Punched Telespar" stateful
+go run client.go app1_op1 app1_op2 TrafficSigns_10.txt h.txt 3 "No Outlet" stateless && 
+go run client.go app2_op1 app2_op2 TrafficSigns_50.txt b.txt 3 "Punched Telespar" stateful 
 echo "All done"
--- mp4/test2.sh
+++ mp4/test2.sh
@@ -1,10 +1,46 @@
-go run src/client.go app1_op1.go app1_op2.go Traffic_Signs_1000.txt test2_1.txt 3 "No Outlet" stateless
+cd src/ 
+go run mp3_client.go create --localfilename ~/TrafficSigns_50.txt --HyDFSfilename TrafficSigns_50.txt && \
+go run client.go app1_op1 app1_op2 TrafficSigns_50.txt 50.txt 3 "No Outlet" stateless &
+cd ..
 
 # wait to the middle of the steam processing
 sleep 1.5
 # fail two applications
 # applicatoins use port 8091
-host="bohaowu2@fa24-cs425-6610.cs.illinois.edu"
+host="bohaowu2@fa24-cs425-6604.cs.illinois.edu"
+ssh "$host" << 'EOF'
+PORT=8091
+PID=$(lsof -t -i :${PORT})
+# If a PID is found, kill the process
+if [ ! -z "$PID" ]; then
+    echo "Stopping worker with PID $PID..."
+    kill $PID
+    # If needed, force kill
+    # sudo kill -9 $PID
+else
+    echo "No worker found on port ${PORT}."
+fi
+EOF
+echo "Execute ssh $host and kill worker with port" 
+
+host="bohaowu2@fa24-cs425-6604.cs.illinois.edu"
+ssh "$host" << 'EOF'
+PORT=8081
+PID=$(lsof -t -i :${PORT})
+# If a PID is found, kill the process
+if [ ! -z "$PID" ]; then
+    echo "Stopping mp2 with PID $PID..."
+    kill $PID
+    # If needed, force kill
+    # sudo kill -9 $PID
+else
+    echo "No mp2 found on port ${PORT}."
+fi
+EOF
+echo "Execute ssh $host and kill mp2 with port" 
+
+
+host="bohaowu2@fa24-cs425-6607.cs.illinois.edu"
 ssh "$host" << 'EOF'
 PORT=8091
 PID=$(lsof -t -i :${PORT})
@@ -21,22 +57,21 @@ EOF
 echo "Execute ssh $host and kill worker with port" 
 
 
-host="bohaowu2@fa24-cs425-6609.cs.illinois.edu"
+host="bohaowu2@fa24-cs425-6607.cs.illinois.edu"
 ssh "$host" << 'EOF'
-PORT=8091
+PORT=8081
 PID=$(lsof -t -i :${PORT})
 # If a PID is found, kill the process
 if [ ! -z "$PID" ]; then
-    echo "Stopping worker with PID $PID..."
+    echo "Stopping mp2 with PID $PID..."
     kill $PID
     # If needed, force kill
     # sudo kill -9 $PID
 else
-    echo "No worker found on port ${PORT}."
+    echo "No mp2 found on port ${PORT}."
 fi
 EOF
-echo "Execute ssh $host and kill worker with port" 
-
+echo "Execute ssh $host and kill mp2 with port" 
 
 # Print to console any duplicate records that were discarded by a task. 
 # Print the tasks that were rescheduled to the console.
