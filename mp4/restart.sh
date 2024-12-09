c23c45cf5a650095dafd6b81393897f1321f83dc
bohaowu2 <bohaowu2@fa24-cs425-6601.cs.illinois.edu>
2024-12-09 01:19:06 -0600

    Update:

--- mp4/restart.sh
+++ mp4/restart.sh
@@ -1,1 +1,4 @@
 cd ~/cs425g66/mp2 && ./setup.sh && cd ~/cs425g66/mp3 && ./setup.sh
+
+cd ~/cs425g66/mp4/src
+go run mp3_client.go create --localfilename ~/TrafficSigns_50.txt --HyDFSfilename TrafficSigns_50.txt
